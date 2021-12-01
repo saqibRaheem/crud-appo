152b6e5255973ba2006759d23d3313beb1179768
saqibRaheem <saqibraheem1999@gmail.com>
2021-11-30 23:15:41 -0800

    some code

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
+// import axios from "axios";
+// const axios = require("axios");
+
+// const { default: axios } = require("axios");
+// const { response } = require("express");
+
+// const port = 'http://localhost/3000'
+const port = "https://crud-appo.herokuapp.com";
 function abc(){
     let userName = document.getElementById('userName').value;
     let email = document.getElementById('email').value;
-    axios.post("https://crud-appo.herokuapp.com/user",{
+    axios.post(port + '/user',{
         userNmae:userName,email:email
 
      
-    })
+     })
         .then((response)=>{
             console.log(response);
             alert('successFully response')
@@ -17,27 +25,94 @@ function abc(){
             alert("not Responding")
             console.log(error);
         })
+        
+ 
+    }
+ function cba(){
+        axios.get(port +'/users')
     
-}
-function cba(){
     
-    axios.get("https://crud-appo.herokuapp.com/users")
     .then((response)=>{
         console.log(response);
         response.data.forEach((data)=> {
             var saveDAta = `
-            <tr id="${data.userName}">
-            <td></td></tr>
+            <tr id="${data_id}">
+            <td id="userName_">${data.userName}</td>
+            <td id="email_">${data.email}</td>
+            <td><button href="javascript:void(0)" onclick=get_record("${data.userName}","${data.email}","${data._id}");id="edit">EDIT</td>
+            <td><button href="javascript:void(0)" onclick=delet_data("${data._id}"); id="delete">DELETE</td>
+            </tr>
             `
-    document.getElementById("tblper").innerHTML += saveDAta;
-            console.log(data)
+            document.getElementById("tblper").innerHTML += saveDAta;
+            console.log(saveDAta)
             
         });
     })
     .catch((err)=>{
-console.log(err)
+        console.log(err)
     })
     .then(()=>{
         // always exicuted
     })
 }
+function get_record(userName_,email_,_id) {
+    // document.getElementById('userName').value= e.userName;
+    // let id = e._id;
+    // let email =e.email;
+    // let address = e.address;
+
+    // console.log("yar check karo"+userName, user_id, id, document.getElementById('userName_' + id));
+    console.log(userName_);
+    console.log(email_);
+    console.log(_id);
+    console.log("kuch nhi");
+    document.getElementById(_id).innerHTML = `
+    <tr id="${_id}"> 
+        
+         
+         
+            <td><input type="text" id="${_id}-userName_" value="${userName_}" /></td>
+            <td><input type="text" id="${_id}-email_" value="${email_}" /></td>
+            <button type="button" onclick="updateC('${_id}')" class="btn btn-success">Update</button>
+        </tr>`;
+
+    // document.getElementById('userName').value = userName_;
+    // document.getElementById('email').value = email_;
+    document.getElementById('user_id').value = _id;
+}
+
+
+function updateC(_id) {
+    let userName = document.getElementById(`${_id}-userName_`).value;
+    let email = document.getElementById(`${_id}-email_`).value;
+    axios.put("https://crud-opperation.herokuapp.com/user/" + id, {
+        userName: userName,
+        email: email,
+    })
+        .then((response) => {
+            alert("user updated");
+            console.log(response)
+            cba();
+        })
+        .catch((error) => {
+            console.log(error);
+            console.log(err);
+
+        });
+}
+function delet_data(_id){
+    axios.delete('https://crud-opperation.herokuapp.com/user/'+ _id)
+    .then((response)=>{
+        console.log(response);
+        alert(response.data)
+        cba();
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+    .then(()=>{
+        console.log();
+    })
+
+}
+cba();
--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 
 // there are all required variables 
-const express = require('express');
+// const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const server = require('http');
+const express=require("express")
 const app=express();
+const mongoose = require('mongoose');
+// const { stringify } = require('querystring');
 const port = process.env.PORT||3000;
 
+const dbURI = "mongodb+srv://saqib:saqib@cluster0.pvnsa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+//This lline connect with databased
+mongoose.connect(dbURI);
+const CrudUser = mongoose.model('CrudUser' , {
+    userName: String,
+    email:String,
 
+})
 // middle ware
 
 app.use(cors());
@@ -16,36 +26,95 @@ app.use(express.json());
 
 
 
-let users=[];
+// let users=[];
 
 app.use((req,res,next)=>{
-    console.log('my first server');
+    console.log('my first server',req.body);
     next();
 
 })
-app.post('/user',(req,res)=>{
-    if(!req.body.userName || !req.body.email){
-        res.send("invalid data");
-        console.log("nh chalra")
-    }else{
-        const newUser={
-            userName:req.body.userName,
-            email:req.body.email
-        };
-        users.push(newUser);
+app.get("/users", (req, res) => {
+    CrudUser.find({}, (err, data) => {
+        if (!err) {
 
-    }
-})
-app.get("/users",(req,res)=>{
-        if(!err){
-            res.send(users)
+            res.send(data)
         }
-        else{
-            res.status(500).send("nh smjh ara")
+        else {
+            res.status(500).send("Error Excuted")
+        }
+    })
+})
+// app.post('/user',(req,res)=>{
+//     if(!req.body.userName || !req.body.email){
+//         res.send("invalid data");
+//         console.log("nh chalra")
+//     }else{
+//         const newUser={
+//             userName:req.body.userName,
+//             email:req.body.email
+//         };
+//         users.push(newUser);
+
+//     }
+// })
+app.get('/users/:id', (req, res) => {
+    CrudUser.findOne({ _id: req.params.id }, (err, data) => {
+        if (!err) {
+            res.send(data)
+
+        }
+        else {
+            res.status(505).send("Invalid Data")
         }
     })
     
+})
 
+app.post('/user',(req,res)=>{
+    if(!req.body.userName || !req.body.email ) {
+        res.status(400).send("invalid data");
+    }else{
+        const newUser = new CrudUser({
+            userName:req.body.userName,
+            email:req.body.email
+        });
+        newUser.save().then(()=>{
+            console.log('user created success');
+            res.send({message:"users created"})
+        })
+    }
+
+})
+
+app.post('/users/:id' , (req,res)=>{
+let updateObj = {}
+if (req.body.userName){
+    updateObj.userName=req.body.userName
+}
+if(req.body.email){
+    updateObj.email= req.body.email
+}
+CrudUser.findByIdAndUpdate(rwq.params.id,updateObj,{ new: true},
+    (err,data)=>{
+        if(!err){
+            res.send(data)
+        }else{
+            res.status(500).send("error NOt REsponding")
+        }
+    })
+})
+
+app.delete('/user/:id', (req, res) => {
+
+    CrudUser.findByIdAndRemove(req.params.id, (err, data) => {
+        if (!err) {
+            res.send("user deleted")
+        } else {
+            res.status(500).send("error exicuted")
+        }
+    })
+
+})
 app.listen(port,()=>{
     console.log(`server is running on ${port} port`)
 })
